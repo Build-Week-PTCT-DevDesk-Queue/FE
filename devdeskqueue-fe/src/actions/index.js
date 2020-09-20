543e410565443750fd24e3a28adba3c43693aad5
mikecrbenton <mikecrbenton@hotmail.com>
2020-09-20 15:09:33 -0500

    push up for sharing reducer

--- devdeskqueue-fe/src/actions/index.js
+++ devdeskqueue-fe/src/actions/index.js
@@ -1,16 +1,6 @@
 import axios from "axios";
 import axiosWithAuth from "../utils/AxiosWithAuth";
 
-export const initializeSmurf = () => {
-   return dispatch => {
-      //console.log("IN DISPATCH FOR INITIALIZE SMURF")   
-       axios.get("http://localhost:3333/smurfs")
-         .then( (response) => {
-            dispatch({ type: "INITIALIZE_SMURF", payload: response.data });
-         })
-         .catch(err => console.log("ERROR:", err));
-     };
-}
 
 // GETs all tickets with helper_id null && status 0 =========================//
 export const getTickets = () => {
