5ffbbe45db9f1a1fe6f9783d873dff5ddb56e489
mikecrbenton <mikecrbenton@hotmail.com>
2020-09-24 18:38:49 -0500

    comments on actions

--- devdeskqueue-fe/src/components/actions/ticketActions.js
+++ devdeskqueue-fe/src/components/actions/ticketActions.js
@@ -2,19 +2,65 @@ import axiosWithAuth from '../../utils/AxiosWithAuth'
 
 
 // GETs all tickets with helper_id null && status 0 =========================//
-export const getTickets = () => {
-   //console.log("get tickets is being called")
-   return dispatch => {
-       axiosWithAuth().get("/api/tickets")
+export const getTickets = () => dispatch => {
+
+   console.log("get tickets is being called")
+
+   //return dispatch => {
+       axiosWithAuth().get("/api/auth/tickets")
          .then( (response) => {
             console.log("GET IN ACTIONS", response )
             dispatch({ type: "GET_TICKETS", payload: response.data });
          })
          .catch(err => console.log("ERROR:", err));
-     };
+     //};
 }
 
-// export const createTicket = () => {
+// HELPER BRANCH - ASSIGN====================================================================
+export const assignTicket = (ticketId, helperId) => {
+   return dispatch => {
+
+      // axiosWithAuth().put(`/api/tickets/${ticketId}/helper/${helperId}`)
+      // .then( (response) => {
+            //console.log(response)
+      //    //dispatch({ type: "GET_TICKETS_BY_HELPER_ID", payload: response.SHAPEOFTHEDATA });
+      // })
+      // .catch(err => console.log("ERROR:", err));
+
+      dispatch( { type: "ASSIGN_TICKET", payload: { ticketId : ticketId , helperId : helperId } } )
+   }
+}
+
+// HELPER BRANCH - RESOLVE ======================================================
+export const resolveTicket = (ticketId) => {
+   return dispatch => {
+
+      // axiosWithAuth().put("/api/tickets/${ticketId}/status", { "status": 1 } )
+      // .then( (response) => {
+            //console.log(response)
+      //    //dispatch({ type: "GET_TICKETS", payload: response.data });
+      // })
+      // .catch(err => console.log("ERROR:", err));
+
+      dispatch( { type: "RESOLVE_TICKET", payload: ticketId } )
+   }
+}
+
+// HELPER BRANCH - REASSIGN ============================================
+export const reassignTicket = (ticketId) => {
+   return dispatch => {
+   
+      // axiosWithAuth().put(`/api/tickets/${ticketId}/helper/${0}`)
+      // .then( (response) => {
+            //console.log(response)
+      //    //dispatch({ type: "GET_TICKETS", payload: response.data });
+      // })
+      // .catch(err => console.log("ERROR:", err));
+
+      dispatch( { type: "REASSIGN_TICKET", payload: ticketId } )
+   }
+
+   // export const createTicket = () => {
 
   
 // }
@@ -33,45 +79,4 @@ export const getTickets = () => {
 //            dispatch({ type: ADD_TICKET_FAIL, payload: err.response });
 //        });
 // };
-
-export const assignTicket = (ticketId, helperId) => {
-   return dispatch => {
-
-      // axiosWithAuth().put(`/api/tickets/${ticketId}/helper/${helperId}`)
-      // .then( (response) => {
-            //console.log(response)
-      //    //dispatch({ type: "GET_TICKETS_BY_HELPER_ID", payload: response.SHAPEOFTHEDATA });
-      // })
-      // .catch(err => console.log("ERROR:", err));
-
-      dispatch( { type: "ASSIGN_TICKET", payload: { ticketId : ticketId , helperId : helperId } } )
-   }
-}
-
-export const resolveTicket = (ticketId) => {
-   return dispatch => {
-
-      // axiosWithAuth().put("/api/tickets/${ticketId}/status", { "status": 1 } )
-      // .then( (response) => {
-            //console.log(response)
-      //    //dispatch({ type: "GET_TICKETS", payload: response.data });
-      // })
-      // .catch(err => console.log("ERROR:", err));
-
-      dispatch( { type: "RESOLVE_TICKET", payload: ticketId } )
-   }
-}
-
-export const reassignTicket = (ticketId) => {
-   return dispatch => {
-   
-      // axiosWithAuth().put(`/api/tickets/${ticketId}/helper/${0}`)
-      // .then( (response) => {
-            //console.log(response)
-      //    //dispatch({ type: "GET_TICKETS", payload: response.data });
-      // })
-      // .catch(err => console.log("ERROR:", err));
-
-      dispatch( { type: "REASSIGN_TICKET", payload: ticketId } )
-   }
 }
